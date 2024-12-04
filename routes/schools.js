30a45f821810a513a8816f19eb5111693f656e44
Anand_Shete <anandshete1234@gmail.com>
2024-12-04 08:05:32 +0530

    first commit

--- routes/schools.js
+++ routes/schools.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const db = require('../connect');
+const db = require('../database/connect');
 
 // Add School API
 router.post('/addSchool', async (req, res) => {
